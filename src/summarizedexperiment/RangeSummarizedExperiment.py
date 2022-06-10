e2bc80dbc0c7e2c7dd06528536b31c11e5415ed6
jkanche <jayaram.kancherla@gmail.com>
2022-06-10 14:11:47 -0700

    init: create SE and RSE classes

---
+++ src/summarizedexperiment/RangeSummarizedExperiment.py
@@ -0,0 +1,92 @@
+from typing import Any, Dict, Union
+from scipy import sparse as sp
+import numpy as np
+import pandas as pd
+from genomicranges import GenomicRanges
+from .SummarizedExperiment import SummarizedExperiment
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+
+class RangeSummarizedExperiment(SummarizedExperiment):
+    """SummarizedExperiment class to represent genomic experiment data,
+    features, sample data and any other metadata
+    """
+
+    def __init__(
+        self,
+        rows: GenomicRanges,
+        assays: Dict[str, Union[np.ndarray, sp.spmatrix]],
+        cols: pd.DataFrame,
+        metadata: Any = None,
+    ) -> None:
+        """Initialize an instance of `RangeSummarizedExperiment`
+
+        Args:
+            rows (pd.DataFrame): features
+            assays (Dict[str, Union[np.ndarray, sp.spmatrix]]): list of matrices,
+                represented as dense (numpy) or sparse (scipy) matrices
+            cols (pd.DataFrame): sample metadata
+            metadata (Any, optional): experiment metadata describing the
+                methods. Defaults to None.
+        """
+        super().__init__(rows, assays, cols, metadata=metadata)
+
+    def rowRanges(self) -> GenomicRanges:
+        """Accessor to retrieve features
+
+        Returns:
+            GenomicRanges: returns features in the container
+        """
+        return self.rows
+
+    def __getitem__(self, args: tuple) -> "RangeSummarizedExperiment":
+        """Subset a SummarizedExperiment
+
+        Args:
+            args (tuple): indices to slice. tuple can
+                contains slices along dimensions
+
+        Returns:
+            RangeSummarizedExperiment: new SummarizedExperiment object
+        """
+        rowIndices = args[0]
+        colIndices = None
+        if len(args) > 1:
+            colIndices = args[1]
+
+        new_rows = None
+        new_cols = None
+        new_assays = None
+
+        if rowIndices is not None:
+            new_rows = self.rows[rowIndices]
+
+        if colIndices is not None:
+            new_cols = self.cols.iloc[colIndices]
+
+        new_assays = self.subsetAssays(
+            rowIndices=rowIndices, colIndices=colIndices
+        )
+
+        return RangeSummarizedExperiment(
+            new_rows, new_assays, new_cols, self.metadata
+        )
+
+    def subsetByOverlaps(
+        self, query: GenomicRanges
+    ) -> "RangeSummarizedExperiment":
+        """Subset a RangeSummarizedExperiment by feature overlaps
+
+        Args:
+            query (GenomicRanges): query genomic intervals
+
+        Raises:
+            NotImplementedError: Currently not implemented
+
+        Returns:
+            RangeSummarizedExperiment: new SummarizedExperiment object
+        """
+        raise NotImplementedError
---
+++ tests/test_SE.py
@@ -0,0 +1,67 @@
+import pytest
+
+from summarizedexperiment import SummarizedExperiment
+from genomicranges import GenomicRanges
+import numpy as np
+from random import random
+import pandas as pd
+from summarizedexperiment.RangeSummarizedExperiment import RangeSummarizedExperiment as rse
+from summarizedexperiment.SummarizedExperiment import SummarizedExperiment as se
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+
+nrows = 200
+ncols = 6
+counts = np.random.rand(nrows, ncols)
+df_gr = pd.DataFrame(
+    {
+        "seqnames": [
+            "chr1",
+            "chr2",
+            "chr2",
+            "chr2",
+            "chr1",
+            "chr1",
+            "chr3",
+            "chr3",
+            "chr3",
+            "chr3",
+        ]
+        * 20,
+        "starts": range(100, 300),
+        "ends": range(110, 310),
+        "strand": ["-", "+", "+", "*", "*", "+", "+", "+", "-", "-"] * 20,
+        "score": range(0, 200),
+        "GC": [random() for _ in range(10)] * 20,
+    }
+)
+
+gr = GenomicRanges.fromPandas(df_gr)
+
+colData = pd.DataFrame(
+    {
+        "treatment": ["ChIP", "Input"] * 3,
+    }
+)
+
+
+def test_SE_creation():
+    tse = SummarizedExperiment(
+        assays={"counts": counts}, rowData=df_gr, colData=colData
+    )
+
+    assert tse is not None
+    assert isinstance(tse, se)
+
+
+def test_RSE_creation():
+
+    trse = SummarizedExperiment(
+        assays={"counts": counts}, rowRanges=gr, colData=colData
+    )
+
+    assert trse is not None
+    assert isinstance(trse, rse)

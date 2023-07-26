6c30ae71c5c1c4b0d72b66b4c98c88ef52112cdd
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-07-26 16:31:44 -0700

    some healthy modifications to search

--- src/summarizedexperiment/utils/_slicer.py
+++ src/summarizedexperiment/utils/_slicer.py
@@ -1,39 +1,54 @@
 from typing import Any, Sequence
 
-import numpy as np
 import pandas as pd
 
 __author__ = "keviny2, jkanche"
-__copyright__ = "Genentech"
+__copyright__ = "jkanche"
 __license__ = "MIT"
 
 
 def get_indexes_from_names(
-    source: Sequence[Any], target: Sequence[Any]
+    source: Sequence[Any], target: Sequence[Any], firstMatch: bool = True
 ) -> Sequence[int]:
     """Return the index of the first occurrence of each value in `source`.
 
     Args:
-        source (Sequence[Any]): the list in which to search for the values.
-        target (Sequence[Any]): the list of values to find indices for.
+        source (Sequence[Any]): list in which to search for the values.
+        target (Sequence[Any]): list of values to find indices for.
+        firstMatch (bool): only return first matches? Defaults to True.
 
     Raises:
         ValueError: if any value in `target` is not found in `source`.
+        ValueError: if target contains duplicates.
 
     Returns:
         Sequence[int]: the indexes of the first occurrence of each value in
             `target` within `source`.
     """
-    missing_names = set(target).difference(source)
-    if len(missing_names) > 0:
-        raise ValueError("invalid index names(s): " + ", ".join(missing_names))
 
-    value_to_index = {}
-    for index, value in enumerate(source):
-        if value not in value_to_index:
-            value_to_index[value] = index
-    
-    return [value_to_index[value] for value in target]
+    if isinstance(source, pd.Index):
+        source = source.tolist()
+
+    if isinstance(target, pd.Index):
+        target = target.tolist()
+
+    set_target = set(target)
+
+    missing_names = set_target.difference(source)
+    if len(missing_names) > 0:
+        raise ValueError(f"invalid index names(s): {', '.join(missing_names)}")
+
+    if len(set_target) != len(target):
+        raise ValueError("target contains duplicate values.")
+
+    if firstMatch is True:
+        return [source.index(x) for x in target]
+
+    match_indices = []
+    for v in target:
+        match_indices.extend([i for i, x in enumerate(source) if x == v])
+
+    return list(set(match_indices))
 
 
 def get_indexes_from_bools(x: Sequence[bool], match: bool = True) -> Sequence[int]:

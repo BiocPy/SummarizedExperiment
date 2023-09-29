e12d9cfb2a5b8ea84b79f8b9851ffc4c91f9df24
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-09-29 08:07:32 -0700

    Eliminate unnecessary dependency on filebackedarray. (#45)

--- src/summarizedexperiment/types.py
+++ src/summarizedexperiment/types.py
@@ -4,14 +4,13 @@ from typing import List, Tuple, Union
 import numpy as np
 import pandas as pd
 from biocframe import BiocFrame
-from filebackedarray import H5BackedDenseData, H5BackedSparseData
 from scipy import sparse as sp
 
 __author__ = "jkanche"
 __copyright__ = "jkanche"
 __license__ = "MIT"
 
-MatrixTypes = Union[np.ndarray, sp.spmatrix, H5BackedSparseData, H5BackedDenseData]
+MatrixTypes = Union[np.ndarray, sp.spmatrix]
 ArrayTypes = Union[np.ndarray, sp.lil_matrix]
 BiocOrPandasFrame = Union[pd.DataFrame, BiocFrame]
 MatrixSlicerTypes = Union[List[int], List[bool], slice]
